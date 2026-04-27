ea160071d5e45461657b64a4b691dfe7fcc4ff65
ArchiScan <dev@archiscan.app>
2026-04-27 11:21:15 +0000

    revert: next.config 원복 (webpack 설정 오류로 빌드 실패)

--- next.config.mjs
+++ next.config.mjs
@@ -6,19 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Chunk 파일명에서 '..' 제거 (Samsung Internet 호환)
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      const originalFilename = config.output.chunkFilename
-      config.output.chunkFilename = (pathData) => {
-        const name = typeof originalFilename === 'function'
-          ? originalFilename(pathData)
-          : originalFilename || '[id].js'
-        return name.replace(/\.\./g, '-')
-      }
-    }
-    return config
-  },
   env: {
     MOLIT_API_KEY: process.env.MOLIT_API_KEY || '384c065c489b613aa46ae60dbc3284d59c52d1cbb9ec32bfeba5d56d21444098',
     JUSO_API_KEY: process.env.JUSO_API_KEY || 'U01TX0FVVEgyMDI1MDcyMjE2NTcxMjExNTU0MDc=',
